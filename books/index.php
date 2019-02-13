620edf83d54adbf0f16151b31fd3bf5c6d6c7491
bayev.alen <bayev.alen@yandex.kz>
2019-02-13 22:26:35 +0600

    data

--- books/index.php
+++ books/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Книги.</title>
+		<title>Книги</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
