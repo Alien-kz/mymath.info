7b7cd14e95cdfbfe150db70a8c5848cb69d3ed4a
Alen Bayev <bayev.alen@yandex.kz>
2019-11-23 08:33:15 +0600

    mes

--- student/cmc/files/client.c
+++ student/cmc/files/client.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <unistd.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include <unistd.h>
+
+enum errors {
+    OK,
+    ERR_INCORRECT_ARGS,
+    ERR_SOCKET,
+    ERR_CONNECT
+};
 
 int init_socket(const char *ip, int port) {
     //open socket, result is socket descriptor
     int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Fail: open socket");
+        _exit(ERR_SOCKET);
+    }
 
     //prepare server address
     struct hostent *host = gethostbyname(ip);
@@ -25,18 +37,27 @@ int init_socket(const char *ip, int port) {
     sin.sin_port = htons(port);
     memcpy(&sin.sin_addr, host->h_addr_list[0], sizeof(sin.sin_addr));
     if (connect(server_socket, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
-        perror("connect");
-        return -1;
+        perror("Fail: connect");
+        _exit(ERR_CONNECT);
     }
     return server_socket;
 }
 
-int main() {
-    int server = init_socket("127.0.0.1", 8080);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        puts("Incorrect args.");
+        puts("./server <ip> <port>");
+        puts("Example:");
+        puts("./server 127.0.0.1 5005");
+        return ERR_INCORRECT_ARGS;
+    }
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int server = init_socket(ip, port);
     char data[4];
     for (int i = 0; i < 4; i++) {
         read(server, &data[i], 1);
         printf("%d\n", data[i]);
     }
-    return 0;
+    return OK;
 }
--- student/cmc/files/server.c
+++ student/cmc/files/server.c
@@ -1,36 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#include <unistd.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main() {
-    //open socket, result is socket descriptor
+enum errors {
+    OK,
+    ERR_INCORRECT_ARGS,
+    ERR_SOCKET,
+    ERR_SETSOCKETOPT,
+    ERR_BIND,
+    ERR_LISTEN
+};
+
+int init_socket(int port) {
+    //open socket, return socket descriptor
     int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Fail: open socket");
+        _exit(ERR_SOCKET);
+    }
  
     //set socket option
     int socket_option = 1;
-    setsockopt(server_socket, 
-               SOL_SOCKET, 
-               SO_REUSEADDR, 
-               &socket_option, 
-               sizeof(socket_option));
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, sizeof(socket_option));
+    if (server_socket < 0) {
+        perror("Fail: set socket options");
+        _exit(ERR_SETSOCKETOPT);
+    }
 
     //set socket address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
+    server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
-    bind(server_socket, 
-         (struct sockaddr *) &server_address, 
-         sizeof(server_address));
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+        perror("Fail: bind socket address");
+        _exit(ERR_BIND);
+    }
 
     //listen mode start
-    listen(server_socket, 5);
+    if (listen(server_socket, 5) < 0) {
+        perror("Fail: bind socket address");
+        _exit(ERR_LISTEN);
+    }
+    return server_socket;
+}
 
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        puts("Incorrect args.");
+        puts("./server <port>");
+        puts("Example:");
+        puts("./server 8000");
+        return ERR_INCORRECT_ARGS;
+    }
+    int port = atoi(argv[1]);
+    int server_socket = init_socket(port);
     while(1) {
         puts("Wait for connection");
         struct sockaddr_in client_address;
@@ -46,5 +77,5 @@ int main() {
     }
 
 
-    return 0;
+    return OK;
 }
